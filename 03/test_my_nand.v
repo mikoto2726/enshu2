d8c98d7ee9731f16d986cb3eed612c030ceb1b4f
1f10220137 <s1f102201379@iniad.org>
2024-10-08 13:27:08 +0900

    test_my_nand

---
+++ 03/test_my_nand.v
@@ -0,0 +1,14 @@
+module test_my_nand;
+reg in1, in2;
+wire out;
+my_nand mn1 (.a(in1), .b(in2), .y(out));
+initial begin
+    $monitor ("%t: in1 = %b, in2 = %b, out = %b", $time, in1, in2, out);
+    in1 = 0; in2 = 0;
+    #10 in2 = 1;
+    #10 in1 = 1; in2 = 0;
+    #10 in1 = 1; in2 = 1;
+    #10 in1 = 0; in2 = 0;
+    $finish;
+end
+endmodule
