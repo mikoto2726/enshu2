445816338f7f8780e2a69b44bd34ca85d87b7c2f
1f10220137 <s1f102201379@iniad.org>
2024-10-08 13:50:58 +0900

    test_my_nand.vcd

--- 03/test_my_nand.v
+++ 03/test_my_nand.v
@@ -3,6 +3,8 @@ reg in1, in2;
 wire out;
 my_nand mn1 (.a(in1), .b(in2), .y(out));
 initial begin
+    $dumpfile("test_my_nand.vcd");
+    $dumpvars(0, test_my_nand);
     $monitor ("%t: in1 = %b, in2 = %b, out = %b", $time, in1, in2, out);
     in1 = 0; in2 = 0;
     #10 in2 = 1;
